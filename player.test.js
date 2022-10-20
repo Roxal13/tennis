7748d03cdde7baad69ea56580949dfb8dec63e3d
Alvaro <alvaroromfra@gmail.com>
2022-10-20 20:11:02 +0100

    Archivos player

---
+++ player.test.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+const Player = require('./player');
+
+test('Crear jugador instancia de Player', () => {
+  const player1 = new Player();
+  expect(typeof player1).toBe('object');
+  expect(player1).toBeInstanceOf(Object);
+});
+
+test('Crear jugador con nombre, se almacena nombre en propiedad name', () => {
+  const player1 = new Player('Pepe');
+	expect(player1.name).toBe('Pepe');
+});
