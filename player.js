4b50208a1ca0b4955c97277167f01ee6c08d8470
Alvaro <alvaroromfra@gmail.com>
2022-10-26 14:57:24 +0100

    Actualizacion Player, creacion clase Match

--- player.js
+++ player.js
@@ -3,6 +3,8 @@ class Player {
   name;
   #gender = 'female';
   birthDate;
+  age;
+  category;
 
   constructor(name, gender, date) {
     this.name = name;
@@ -12,14 +14,24 @@ class Player {
     if (date !== null && date !== undefined) {
       const [d, m, y] = date.split('/');
       this.birthDate = new Date(d, m, y);
+      const today = new Date();
+      this.age = today.getFullYear() - this.birthDate.getFullYear();
+      const monthDiff = today.getMonth() - this.birthDate.getMonth();
+      if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < this.birthDate.getDate())) {
+        this.age -= 1;
+      }
     }
   }
   get gender() {
     return this.#gender;
   }
-
   // eslint-disable-next-line class-methods-use-this, no-empty-function
   set gender(gender) {}
+
+  // eslint-disable-next-line no-dupe-class-members
+  get age() {
+    return this.age;
+  }
 
   setBirthDate(date) {
     this.birthDate = date;
